bcaf0b5429f1d4d52855e45d907300cc469864a5
Nick Fischer <nick.korotkin@outlook.com>
2025-05-27 22:33:17 +0200

    Update power.nix

--- nixos/modules/power.nix
+++ nixos/modules/power.nix
@@ -3,28 +3,4 @@
 }: {
   powerManagement.enable = true;
   services.thermald.enable = true;
-
-  #services.tlp.enable = false;
-
-  # Needed for e.g. AGS
-  # TODO Needed?
-  #services.upower.enable = true;
-
-  # TODO Uhm, does this work at all?
-  #services.auto-cpufreq = {
-  #  enable = false; # TODO This conflicts with gnome.. what is better?
-
-  #  settings = {
-  #    battery = {
-  #      governor = "powersave";
-  #      energy_performance_preference = "balance-performance";
-  #      turbo = "never";
-  #    };
-  #    charger = {
-  #      governor = "performance";
-  #      energy_performance_preference = "performance";
-  #      turbo = "auto";
-  #    };
-  #  };
-  #};
 }
