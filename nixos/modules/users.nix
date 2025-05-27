09f46f1e33f0662b3587d285c4cb1116b74aec01
Nick Fischer <nick.korotkin@outlook.com>
2025-05-27 22:05:45 +0200

    Update users.nix

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -7,7 +7,7 @@
   users.mutableUsers = false;
   users.users."nick" = {
     isNormalUser = true;
-    shell = pkgs.nushell;
+    # shell = pkgs.nushell;
     initialHashedPassword = "$y$j9T$8fEFURYXvsFCcIcPX5/4o0$U91T.17uI95SIWlerbecLpcek1VSEcYYrTH/2LDAJw.";
     extraGroups = [ "wheel" "wireshark" "firejail" "libvirtd" ];
   };
