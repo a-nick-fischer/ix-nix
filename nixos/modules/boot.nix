abaa16e238a23317a966631d79819f48b27cd857
Nick Fischer <me@nifi.blog>
2026-03-25 20:36:59 +0100

    Styling limine

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -37,6 +37,7 @@ in {
 
         style = {
           interface.branding = "";
+          wallpapers = [ ];
           graphicalTerminal = {
             palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
             background = "1e1e2e";
@@ -44,6 +45,7 @@ in {
             brightPalette = "585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
             brightBackground = "585b70";
             brightForeground = "cdd6f4";
+            font.scale = "2x2";
           };
         };
 
