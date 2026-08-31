d4fa5564cf863e8fe2aaafa908cf4bf4a7cbd6ee
Nick Fischer <me@nifi.blog>
2026-08-31 13:00:24 +0200

    Remove jan

--- nixos/modules/overlays.nix
+++ nixos/modules/overlays.nix
@@ -73,14 +73,14 @@
         desktop = "${prev.kitty}/share/applications/kitty.desktop";
       };
 
-      jan = nvidiaOffloadWrap {
-        executable = "${prev.jan}/bin/Jan";
-        desktop = "${prev.jan}/share/applications/Jan.desktop";
+      krita = nvidiaOffloadWrap {
+        executable = "${prev.krita}/bin/krita";
+        desktop = "${prev.krita}/share/applications/org.kde.krita.desktop";
       };
 
       tor-browser = firejailWrap {
         executable = "${prev.tor-browser}/bin/tor-browser";
-        desktop = "${prev.tor-browser}/share/applications/torbrowser.desktop";
+        desktop = "${prev.tor-browser}/share/applications/tor-browser.desktop";
       };
     })
   ];
